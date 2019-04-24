b03678fae1098c31a4ef780ef8e8c9e575f7c177
alvinhui <alvin.hui@qq.com>
2019-04-24 15:00:33 +0800

    refactor: store 轻量化

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
-# fiy[WIP]
+# fiy
 
-基于 React Hooks 的数据流方案
+基于 React Hooks 的数据流方案。
+
+## 如何使用
+
+### 声明 Store
+
+```javascript
+// src/stores/materials.js
+export default {
+  dataSource: [],
+  async refresh() {
+    this.dataSourde = await fetch(/* api */);
+  },
+  add(project) {
+    this.dataSourde = [].concat(this.dataSource).concat([project]) };
+  },
+  async action() {
+    // ...
+  },
+  actionSync() {
+    // ...
+  },
+};
+```
+
+### 注册 Store
+
+```javascript
+// src/stores/index.js
+import react from 'react';
+import projects from './materials';
+import Fiy from 'fiy';
+
+const fiy = new Fiy(react);
+fiy.registerStore('materials', materials);
+
+export default fiy;
+```
+
+### 在 View 中使用
+
+```javascript
+import React, { useEffect } from 'react';
+import stores from '@src/stores';
+
+const Material = () => {
+  const materials = stores.useStore('materials');
+  const {  dataSource } = materials;
+
+  useEffect(() => {
+    (async () => {
+      await materials.refresh();
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h2>Material</h2>
+      <div>
+        {dataSource.map(({ name }) => name)}
+      </div>
+    </div>
+  );
+};
+
+export default Material;
+```
--- src/store.ts
+++ src/store.ts
@@ -1,25 +1,6 @@
 const isFunction = require('lodash.isfunction');
-
-interface IAction {
-  type: string, 
-  payload?: any[], 
-  loading?: boolean
-}
-
-const isPromise = (fn) => {
-  if (fn instanceof Promise) {
-    return true;
-  }
-  if (typeof fn === 'object' && typeof fn.then === 'function') {
-    return true;
-  }
-  return false;
-};
-
 export default class Store {
   private state: {[name: string]: any} = {};
-
-  private originalMethods = {};
 
   private methods = {};
 
@@ -30,72 +11,26 @@ export default class Store {
       const value = bindings[key];
 
       if (isFunction(value)) {
-        this.originalMethods[key] = value;
-        this.methods[key] = this.createMethod(key);
+        this.methods[key] = this.createMethod(value);
       } else {
         this.state[key] = value;
       }
     }
   } 
 
-  private createMethod(type: string) {
-    const method = (...args) => {
-      this.dispatch({ type, payload: args });
-      if (method.loading) {
-        method.promise
-          .then(payload => this.dispatch({ type, payload, loading: false }))
-          .catch(() => this.dispatch({ type, loading: false }));
-        return method.promise;
-      }
-      return this.state;
+  private createMethod(fun) {
+    return async (...args) => {
+      const newState = {...this.state};
+      await fun.apply(newState, args);
+      this.setState(newState);
     };
-    method.loading = false;
-    method.promise = null;
-
-    return method;
   }
 
-  private dispatch(action: IAction): void {
-    const newState = { ...this.state, ...this.getNextState(action) };
-    if (!this.shouldUpdate(newState)) {
-      return;
-    }
-
+  private setState(newState) {
     this.state = newState;
     const queue = [].concat(this.queue);
     this.queue = [];
     queue.forEach(setState => setState(newState));
-  }
-
-  private getNextState(action: IAction) {
-    const { type, payload = {}, loading } = action;
-    const originalMethod = this.originalMethods[type];
-    const method = this.methods[type];
-
-    if (!originalMethod) {
-      return this.state;
-    }
-
-    if (loading === false && method.loading) {
-      method.loading = false;
-      return payload;
-    }
-
-    const newState = {...this.state};
-    const result = originalMethod.apply(newState, payload);
-    if (!isPromise(result)) {
-      return newState;
-    }
-
-    method.loading = true;
-    method.promise = result;
-
-    return this.state;
-  }
-
-  // TODO
-  private shouldUpdate(newState): boolean {
-    return true;
   }
 
   public useStroe() {
