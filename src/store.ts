50ce3658cc65b1600ef3633d422b323ce24eab1f
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2019-07-01 19:52:16 +0800

    fix: change eslint indent rule to 2 spaces

--- src/store.ts
+++ src/store.ts
@@ -3,56 +3,56 @@ import * as isFunction from 'lodash.isfunction';
 import { useState, useEffect } from 'react';
 
 interface MethodFunc {
-    (): void;
+  (): void;
 }
 
 export default class Store {
-    private state: {[name: string]: any} = {};
+  private state: {[name: string]: any} = {};
 
-    private methods: {[name: string]: MethodFunc} = {};
+  private methods: {[name: string]: MethodFunc} = {};
 
-    private queue = [];
+  private queue = [];
 
-    public constructor(bindings: object) {
-        Object.keys(bindings).forEach((key) => {
-            const value = bindings[key];
+  public constructor(bindings: object) {
+    Object.keys(bindings).forEach((key) => {
+      const value = bindings[key];
 
-            if (isFunction(value)) {
-                this.methods[key] = this.createMethod(value);
-            } else {
-                this.state[key] = value;
-            }
-        });
-    }
+      if (isFunction(value)) {
+        this.methods[key] = this.createMethod(value);
+      } else {
+        this.state[key] = value;
+      }
+    });
+  }
 
-    private getBindings() {
-        return { ...this.state, ...this.methods };
-    }
+  private getBindings() {
+    return { ...this.state, ...this.methods };
+  }
 
-    private createMethod(fun): MethodFunc {
-        return async (...args) => {
-            const newState = { ...this.state };
-            await fun.apply(newState, args);
-            this.setState(newState);
-            return this.getBindings();
-        };
-    }
+  private createMethod(fun): MethodFunc {
+    return async (...args) => {
+      const newState = { ...this.state };
+      await fun.apply(newState, args);
+      this.setState(newState);
+      return this.getBindings();
+    };
+  }
 
-    private setState(newState: object): void {
-        this.state = newState;
-        this.queue.forEach(setState => setState(newState));
-    }
+  private setState(newState: object): void {
+    this.state = newState;
+    this.queue.forEach(setState => setState(newState));
+  }
 
-    public useStore(): object {
-        const [, setState] = useState(this.state);
-        useEffect(() => {
-            this.queue.push(setState);
-            return () => {
-                const index = this.queue.indexOf(setState);
-                this.queue.splice(index, 1);
-            };
-        }, []);
+  public useStore(): object {
+    const [, setState] = useState(this.state);
+    useEffect(() => {
+      this.queue.push(setState);
+      return () => {
+        const index = this.queue.indexOf(setState);
+        this.queue.splice(index, 1);
+      };
+    }, []);
 
-        return this.getBindings();
-    }
+    return this.getBindings();
+  }
 }
