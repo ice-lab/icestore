c5893437d58d317b46aeefdbdbe361e4f9314d23
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2019-07-03 17:37:47 +0800

    feat: add util test

--- src/util.ts
+++ src/util.ts
@@ -4,29 +4,30 @@ import * as isFunction from 'lodash.isfunction';
 const { isArray } = Array;
 
 /**
- * Recursively add proxy to javascipt variable
- * @param {*} value - javascript variable of any type
- * @param {func} handler - proxy handler
+ * Recursively add proxy to object and array
+ * @param {*} value - variable of any type
+ * @param {object} handler - proxy handler
  * @return {object} new proxy object
  */
 /* eslint no-param-reassign: 0, import/prefer-default-export: 0 */
-export const addProxy = (value, handler) => {
+export function addProxy(value: any, handler: object): any {
+  // basic type (number, boolean, string, null, undefined) and function type
   if (!isObject(value) || isFunction(value)) {
     return value;
   }
 
-  if (isArray(value)) {
+  if (isArray(value)) { // array type
     value.forEach((item, index) => {
-      if (isObject(item)) {
+      if (isObject(item)) { // object and array type
         value[index] = addProxy(item, handler);
       }
     });
-  } else if (isObject(value)) {
+  } else if (isObject(value)) { // object type
     Object.keys(value).forEach((key) => {
-      if (isObject(value[key])) {
+      if (isObject(value[key])) { // object and array type
         value[key] = addProxy(value[key], handler);
       }
     });
   }
   return new Proxy(value, handler);
-};
+}
