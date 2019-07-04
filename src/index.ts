326c920fdb89e120c38cebaa777e320bac6e3a99
zhanwen.zw <zhanwen.zw@alibaba-inc.com>
2019-07-04 08:16:49 +0800

    chore: add comment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import Store from './store';
 
 export default class Icestore {
+  // stores registered
   private stores: {[namespace: string]: Store} = {};
 
+  /**
+   * Register and init store
+   * @param {string} namespace - unique name of store
+   * @param {object} bindings - object of state and actions used to init store
+   * @return {object} store instance
+   */
   public registerStore(namespace: string, bindings: object): Store {
     if (this.stores[namespace]) {
       throw new Error(`Namespace have been used: ${namespace}.`);
@@ -12,6 +19,11 @@ export default class Icestore {
     return this.stores[namespace];
   }
 
+  /**
+   * Find store by namespace
+   * @param {string} namespace - unique name of store
+   * @return {object} store instance
+   */
   private getModel(namespace: string): Store {
     const store: Store = this.stores[namespace];
     if (!store) {
@@ -20,10 +32,20 @@ export default class Icestore {
     return store;
   }
 
+  /**
+   * Hook of using store
+   * @param {string} namespace - unique name of store
+   * @return {object} store's bindings
+   */
   public useStore(namespace: string): object {
     return this.getModel(namespace).useStore();
   }
 
+  /**
+   * Hook of using multiple stores
+   * @param {string} namespace - unique name of store
+   * @return {array} array of store's bindings
+   */
   public useStores(namespaces: string[]): object[] {
     return namespaces.map(namespace => this.useStore(namespace));
   }
