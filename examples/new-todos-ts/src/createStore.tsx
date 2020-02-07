2e98f3ba1d35d930fdaf03b8edc863c9d1a7cd9e
alvinhui <alvin.hui@qq.com>
2020-02-07 15:02:40 +0800

    chore: lint

--- examples/new-todos-ts/src/createStore.tsx
+++ examples/new-todos-ts/src/createStore.tsx
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
 
 export interface ContainerProviderProps<State = void> {
-	initialState?: State
-	children: React.ReactNode
+  initialState?: State;
+  children: React.ReactNode;
 }
 
 export interface Container<Value, State = void> {
-	Provider: React.ComponentType<ContainerProviderProps<State>>
-	useContainer: () => Value
+  Provider: React.ComponentType<ContainerProviderProps<State>>;
+  useContainer: () => Value;
 }
 
 export function createContainer<Value, State = void>(
-	useHook: (initialState?: State) => Value,
+  useHook: (initialState?: State) => Value,
 ): Container<Value, State> {
-	const Context = React.createContext<Value | null>(null)
+  const Context = React.createContext<Value | null>(null);
 
-	function Provider(props: ContainerProviderProps<State>) {
-		const value = useHook(props.initialState)
-		return <Context.Provider value={value}>{props.children}</Context.Provider>
-	}
+  function Provider(props: ContainerProviderProps<State>) {
+    const value = useHook(props.initialState);
+    return <Context.Provider value={value}>{props.children}</Context.Provider>;
+  }
 
-	function useContainer(): Value {
-		const value = React.useContext(Context)
-		if (value === null) {
-			throw new Error("Component must be wrapped with <Container.Provider>")
-		}
-		return value
-	}
+  function useContainer(): Value {
+    const value = React.useContext(Context);
+    if (value === null) {
+      throw new Error('Component must be wrapped with <Container.Provider>');
+    }
+    return value;
+  }
 
-	return { Provider, useContainer };
+  return { Provider, useContainer };
 }
 
 export function createStore(models) {
@@ -51,9 +51,9 @@ export function createStore(models) {
   function Provider({ children }) {
     Object.keys(containers).forEach(namespace => {
       const { Provider: ModelProvider } = containers[namespace];
-      children = <ModelProvider>{children}</ModelProvider>
+      children = <ModelProvider>{children}</ModelProvider>;
     });
-    return <>{children}</>
+    return <>{children}</>;
   }
 
   function useStore(namespace) {
