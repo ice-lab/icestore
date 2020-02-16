a48f178f55883ae2df5b61a0e4ac69bf8f94c504
alvinhui <alvin.hui@qq.com>
2020-02-16 13:28:21 +0800

    chore: class && function component

--- examples/todos/src/components/TodoList.tsx
+++ examples/todos/src/components/TodoList.tsx
@@ -1,56 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import store from '../store';
 
-const { withModel, useModels } = store;
+const { useModel, useModelActionsState } = store;
 
-class TodoList extends Component<any> {
-  onRemove = (index) => {
-    const [, actions] = this.props.model;
-    actions.remove(index);
-  }
-
-  onToggle = (index) => {
-    const [, actions] = this.props.model;
-    actions.toggle(index);
-  }
-
-  render() {
-    const { title, model } = this.props;
-    const [ state, , actionsState ] = model;
-    const { dataSource, subTitle } = state;
-    return (
-      <div>
-        <h2>{title}</h2>
-        <p>
-          Using Class Component, SubTitle is {subTitle}
-        </p>
-        <ul>
-          {dataSource.map(({ name, done = false }, index) => (
-            <li key={index}>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={done}
-                  onChange={() => this.onToggle(index)}
-                />
-                {done ? <s>{name}</s> : <span>{name}</span>}
-              </label>
-              {
-                actionsState.remove.isLoading ?
-                  '...deleting...' :
-                  <button type="submit" onClick={() => this.onRemove(index)}>-</button>
-              }
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+export function TodoList({ title, subTitle, dataSource }, { toggle, remove }, actionsState) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>
+        Now is using {subTitle}.
+      </p>
+      <ul>
+        {dataSource.map(({ name, done = false }, index) => (
+          <li key={index}>
+            <label>
+              <input
+                type="checkbox"
+                checked={done}
+                onChange={() => toggle(index)}
+              />
+              {done ? <s>{name}</s> : <span>{name}</span>}
+            </label>
+            {
+              actionsState.remove.isLoading ?
+                '...deleting...' :
+                <button type="submit" onClick={() => remove(index)}>-</button>
+            }
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
-export default withModel(
-  'todos',
-  (state) => ({ ...state, subTitle: 'SubTitle' }),
-  (actions) => actions,
-  (actionsState) => actionsState,
-)(TodoList);
+export default function({ title }) {
+  const [ state, actions ] = useModel('todos');
+  const actionsState = useModelActionsState('todos');
+  return TodoList(
+    { ...state, title, subTitle: 'Function Component' },
+    actions,
+    actionsState
+  );
+}
---
+++ examples/todos/src/components/TodoListClass.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import store from '../store';
+import { TodoList } from './TodoList';
+
+const { withModel } = store;
+
+class TodoListClass extends Component<any> {
+  onRemove = (index) => {
+    const [, actions] = this.props.model;
+    actions.remove(index);
+  }
+
+  onToggle = (index) => {
+    const [, actions] = this.props.model;
+    actions.toggle(index);
+  }
+
+  render() {
+    const { title, model } = this.props;
+    const [ state, , actionsState ] = model;
+    const { dataSource, subTitle } = state;
+    return TodoList(
+      { title, subTitle, dataSource },
+      { toggle: this.onToggle, remove: this.onRemove },
+      actionsState
+    );
+  }
+}
+
+export default withModel(
+  'todos',
+  (state) => ({ ...state, subTitle: 'Class Component' }),
+  (actions) => actions,
+  (actionsState) => actionsState,
+)(TodoListClass);
