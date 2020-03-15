35f581cc877979c082c280d23fae2757419c6e41
luhc228 <luhengchang228@gmail.com>
2020-03-15 10:52:01 +0800

    test: add createUseContainer test

--- tests/createContainer.spec.tsx
+++ tests/createContainer.spec.tsx
@@ -1,14 +1,17 @@
-import { renderHook } from '@testing-library/react-hooks';
-import { createContainer, createUseContainer } from '../src/createContainer';
+import React from 'react';
+import * as rhl from '@testing-library/react-hooks';
+import { createUseContainer } from '../src/createContainer';
 
 const NO_PROVIDER = '_NP_' as any;
 
 describe('createContainer', () => {
   it('throws if component is not wrapped in provider', () => {
+    const Context = React.createContext(NO_PROVIDER);
 
-    // const useContainer = createUseContainer(NO_PROVIDER);
-    // const { result } = renderHook(() => useContainer());
-    // console.log(result.current, result.error);
-    // expect(result.error).toEqual(Error('Component must be wrapped within a Provider.'));
+    const useContainer = createUseContainer(Context);
+
+    const { result } = rhl.renderHook(() => useContainer());
+
+    expect(result.error.message).toMatch('Component must be wrapped within a Provider.');
   });
 });
