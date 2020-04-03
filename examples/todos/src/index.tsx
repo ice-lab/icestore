eb9885726ad6e043a1d5060befd90aa5382b9bac
许文涛 <alvin.hui@qq.com>
2020-04-03 17:00:21 +0800

    Fix: 1.3.1 (#89)
    * fix: setState action name format same as isListener
    
    * chore: initialState

--- examples/todos/src/index.tsx
+++ examples/todos/src/index.tsx
@@ -6,11 +6,21 @@ import TodoAdd from './components/TodoAdd';
 import User from './components/User';
 import Car from './components/Car';
 
+const initialState = {
+  user: {
+    dataSource: {
+      name: 'Tom',
+    },
+    auth: true,
+    todos: 5,
+  },
+};
+
 const { Provider } = store;;
 
 function App() {
   return (
-    <Provider>
+    <Provider initialState={initialState}>
       <Todos />
       <TodoAdd />
       <User />
