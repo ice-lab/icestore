564ea92909f4ecff78e8e94d8401fc0c68e5749e
alvinhui <alvin.hui@qq.com>
2020-04-22 10:46:57 +0800

    chore: withModel example

--- examples/withModel/src/index.tsx
+++ examples/withModel/src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from '@ice/store';
+import { withModel } from '@ice/store';
 
 const delay = (time) => new Promise((resolve) => setTimeout(() => resolve(), time));
 
-// 1️⃣ Use a model to define your store
 const counter = {
   state: 0,
   reducers: {
@@ -19,17 +18,8 @@ const counter = {
   }),
 };
 
-const models = {
-  counter,
-};
-
-// 2️⃣ Create the store
-const store = createStore(models);
-
-// 3️⃣ Consume model
-const { useModel } = store;
-function Counter() {
-  const [ count, dispatchers ] = useModel('counter');
+function Counter({ model }) {
+  const [ count, dispatchers ] = model.useValue('counter');
   const { increment, decrementAsync } = dispatchers;
   return (
     <div>
@@ -40,15 +30,7 @@ function Counter() {
   );
 }
 
-// 4️⃣ Wrap your components with Provider
-const { Provider } = store;
-function App() {
-  return (
-    <Provider>
-      <Counter />
-    </Provider>
-  );
-}
+const CounterWithModel = withModel(counter)(Counter);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<CounterWithModel />, rootElement);
