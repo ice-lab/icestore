222681bca3e634fad6fafcafa3866fc1e3293c45
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2021-06-16 17:12:46 +0800

    chore: add publish

---
+++ scripts/publish.ts
@@ -0,0 +1,44 @@
+/**
+ * Scripts to check unpublished version and run publish
+ */
+import { execSync } from 'child_process';
+import { join } from 'path';
+import * as fse from 'fs-extra';
+import checkVersionExist from './checkVersionExist';
+
+// Set by github actions
+const branchName = process.env.BRANCH_NAME;
+const rootDir = join(__dirname, '../');
+
+if (!branchName) {
+  throw new Error('Only support publish in GitHub Actions env');
+}
+
+(async () => {
+  const pkgData = await fse.readJSON(join(rootDir, 'package.json'));
+  const { version, name } = pkgData;
+  const npmTag = branchName === 'master' ? 'latest' : 'beta';
+
+  const versionExist = await checkVersionExist(name, version, 'https://registry.npmjs.org/');
+  if (versionExist) {
+    console.log(`${name}@${version} 已存在，无需发布。`);
+    process.exit(0);
+  }
+
+  if (branchName === 'master' && !/\d+\.\d+\.\d+/.test(version)) {
+    throw new Error(`禁止在 master 分支发布非正式版本 ${version}`);
+  }
+
+  if (branchName !== 'master' && /\d+\.\d+\.\d+/.test(version)) {
+    console.log(`非 master 分支 ${branchName}，不发布正式版本 ${version}`);
+    process.exit(0);
+  }
+
+  execSync(`npm publish --tag ${npmTag} --ignore-scripts`, {
+    cwd: rootDir,
+  });
+
+})().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
