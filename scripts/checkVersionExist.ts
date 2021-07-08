24da02801ff1a5dd06774fa7f437e0edee7204c4
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2021-07-08 17:46:58 +0800

    fix(ci): checkVersionExist compatible not exist

--- scripts/checkVersionExist.ts
+++ scripts/checkVersionExist.ts
@@ -1,6 +1,11 @@
 import { getVersions } from 'ice-npm-utils';
 
 export default async function checkVersionExit(name: string, version: string, registry?: string): Promise<boolean> {
-  const versions = await getVersions(name, registry);
-  return versions.indexOf(version) !== -1;
+  try {
+    const versions = await getVersions(name, registry);
+    return versions.indexOf(version) !== -1;
+  } catch(err) {
+    console.error('checkVersionExit error', err);
+    return false;
+  }
 }
