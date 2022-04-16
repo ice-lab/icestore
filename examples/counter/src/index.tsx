2aef65b84bf81958fddfc34a6366cace22b4b4f3
hzyhbk <hzyhbk@163.com>
2022-04-16 16:56:15 +0800

    feat: 通过使用 createModel 工具函数创建 model, 在 effects 使用 this 时可以获得完善的类型提示

--- examples/counter/src/index.tsx
+++ examples/counter/src/index.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from '@ice/store';
+import { createStore, createModel } from '../../../src';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
 const delay = (time) => new Promise((resolve) => setTimeout(() => resolve(), time));
 
-// 1️⃣ Use a model to define your store
-const counter = {
-  state: 0,
+// 1️⃣ Use createModel function to create a model to define your store
+type IState = {
+  count: number;
+};
+const state = {
+  count: 0,
+};
+const counter = createModel({
+  state,
   reducers: {
-    increment: (prevState) => prevState + 1,
-    decrement: (prevState) => prevState - 1,
+    increment: (prevState: IState) => ({ count: prevState.count + 1 }),
+    decrement: (prevState: IState, payload: number) => ({ count: prevState.count - payload }),
   },
   effects: () => ({
-    async asyncDecrement() {
+    async asyncDecrement(payload: number) {
       await delay(1000);
-      this.decrement();
+      this.decrement(payload || 1);
+    },
+    async anotherEffect() {
+      this.asyncDecrement(2);
     },
   }),
-};
+});
 
 const models = {
   counter,
@@ -31,7 +40,7 @@ const store = createStore(models);
 // 3️⃣ Consume model
 const { useModel } = store;
 function Counter() {
-  const [count, dispatchers] = useModel('counter');
+  const [{ count }, dispatchers] = useModel('counter');
   const { increment, asyncDecrement } = dispatchers;
   return (
     <div>
