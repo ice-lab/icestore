01d58f4bf64f61864d8156163365b71163c7a042
luhc228 <luhengchang228@126.com>
2022-05-13 11:01:05 +0800

    fix: types

--- tests/helpers/createHook.tsx
+++ tests/helpers/createHook.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as rhl from "@testing-library/react-hooks";
 
-function createHook(Provider: React.ComponentType<any>, callback: (...args) => any, namespace: string, initialStates?: any) {
+function createHook(Provider: React.ComponentType<any>, callback: (...args: any[]) => any, namespace: string, initialStates?: any) {
   return rhl.renderHook<React.PropsWithChildren<Record<string, any>>, any>(() => callback(namespace), {
     wrapper: (props) => (
       <Provider {...props} initialStates={initialStates}>
